c08909e500e90efa86d824a98d129ffa8d307728
Your NameAnne-Ruth <anneruth02@gmail.com>
2018-10-16 13:31:31 +0200

    made final changes and added comments

--- load_data_fast.py
+++ load_data_fast.py
@@ -1,28 +1,34 @@
-import json, datetime
+import datetime
 import pandas as pd
 from elasticsearch import Elasticsearch, helpers
 
-qdf =  pd.read_csv('data_gv/questions.csv', sep=',', header=None,
+
+# Create a dataframe for all questions of the goeievraag database.
+qdf =  pd.read_csv('data/questions.csv', sep=',', header=None,
                    names=['questionId', 'date', 'userId', 'categoryId',
                   'question', 'description'], quotechar='"',
                    error_bad_lines=False, escapechar='\\', na_filter=False)
 
-adf =  pd.read_csv('data_gv/answers.csv', sep=',', header=None,
+# Create a dataframe for all answers of the goeievraag database.
+adf =  pd.read_csv('data/answers.csv', sep=',', header=None,
                    names=['answerId', 'date', 'userId', 'questionId',
                   'answer', 'thumbsDown', 'thumbsUp',
                   'isBestAnswer'], quotechar='"',
                    error_bad_lines=False, escapechar='\\', na_filter=False)
 
-cdf =  pd.read_csv('data_gv/categories.csv', sep=',', header=None,
+# Create a dataframe for all categories of the goeievraag database.
+cdf =  pd.read_csv('data/categories.csv', sep=',', header=None,
                    names=['categoryId', 'parentId', 'category'],
                    quotechar='"',error_bad_lines=False,
                    escapechar='\\', na_filter=False)
 
-udf =  pd.read_csv('data_gv/users.csv', sep=',', header=None,
+# Create a dataframe for all users of the goeievraag database.
+udf =  pd.read_csv('data/users.csv', sep=',', header=None,
                     names=['userId', 'registrationDate', 'expertise',
                     'bestAnswers'], quotechar='"',
                     error_bad_lines=False, escapechar='\\', na_filter=False)
 
+# Parse all questions and make json documents to prepare for elasticsearch.
 def create_questions_doc(panda):
     docs = []
     for i, row in panda.iterrows():
@@ -43,6 +49,7 @@ def create_questions_doc(panda):
         docs.append(doc)
     return docs
 
+# Parse all answers and make json documents to prepare for elasticsearch.
 def create_answers_doc(panda):
     docs = []
     for i, row in panda.iterrows():
@@ -65,6 +72,7 @@ def create_answers_doc(panda):
         docs.append(doc)
     return docs
 
+# Parse all categories and make json documents to prepare for elasticsearch.
 def create_categories_doc(panda):
     docs = []
     for i, row in panda.iterrows():
@@ -81,6 +89,7 @@ def create_categories_doc(panda):
         docs.append(doc)
     return docs
 
+# Parse all users and make json documents to prepare for elasticsearch.
 def create_users_doc(panda):
     docs = []
     for i, row in panda.iterrows():
@@ -99,36 +108,41 @@ def create_users_doc(panda):
         docs.append(doc)
     return docs
 
+# Connect with elasticsearch
 es = Elasticsearch(hosts=['http://localhost:9200/'])
 
-if es.indices.exists(index="questions"):
-    es.indices.delete(index="questions")#, ignore=[400, 404])
-    print('Previous version of index removed!\nReplacing with new!')
-if es.indices.exists(index="answers"):
-    es.indices.delete(index="answers")#, ignore=[400, 404])
-    print('Previous version of index removed!\nReplacing with new!')
-if es.indices.exists(index="categories"):
-    es.indices.delete(index="categories")#, ignore=[400, 404])
-    print('Previous version of index removed!\nReplacing with new!')
-if es.indices.exists(index="users"):
-    es.indices.delete(index="users")#, ignore=[400, 404])
-    print('Previous version of index removed!\nReplacing with new!')
+# Check if documents with index ind already exists, if so it will be deleted.
+def exists_check(ind):
+    if es.indices.exists(index=ind):
+        es.indices.delete(index=ind)
+        print('Previous version of index',ind,'has been removed')
 
+exists_check("questions")
+exists_check("answers")
+exists_check("categories")
+exists_check("users")
 
+# Load questions in elasticsearch
 print("Loading questions")
 q_docs = create_questions_doc(qdf)
 helpers.bulk(es, q_docs)
+
+# Load answers in elasticsearch
 print("Loading answers")
 a_docs = create_answers_doc(adf)
 helpers.bulk(es, a_docs)
+
+# Load categories in elasticsearch
 print("Loading categories")
 c_docs = create_categories_doc(cdf)
 helpers.bulk(es, c_docs)
+
+# Load users in elasticsearch
 print("Loading users")
 u_docs = create_users_doc(udf)
 helpers.bulk(es, u_docs)
 
-
+# refresh all indexes.
 es.indices.refresh(index='questions')
 es.indices.refresh(index='answers')
 es.indices.refresh(index='categories')
