9188884a88f67674361c43b18ff7f22b21cb4874
Your NameAnne-Ruth <anneruth02@gmail.com>
2018-10-17 15:42:42 +0200

    made answer page which will show after clicking on a question

--- search.py
+++ search.py
@@ -24,5 +24,21 @@ def result():
     res = es.search(index="questions", size=10000, doc_type="question", body=body)
     return render_template("result.html",result = res)
 
+@app.route('/answer/<int:question_Id>')
+def answers(question_Id):
+    q_res = es.get(index="questions", doc_type="question", id=question_Id)
+
+    body={
+        "size": 1000,
+        "query": {
+            "match": {
+                "questionId": question_Id
+            }
+        }
+    }
+
+    a_res = es.search(index="answers", size=10000, doc_type="answer", body=body)
+    return render_template("answers.html",data = q_res,result = a_res)
+
 if __name__ == '__main__':
    app.run(debug = True)
